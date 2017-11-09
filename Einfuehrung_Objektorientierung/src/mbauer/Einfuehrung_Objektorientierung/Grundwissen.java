6a8d36255c63e44427c8c5947c9df0c1f08cf364
mbauer <mbauer@WW820609.dvs-plattling.de>
2017-11-09 14:01:51 +0100

    Test von Schul-PC

--- Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
+++ Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
@@ -4,9 +4,10 @@ package mbauer.Einfuehrung_Objektorientierung;
 public class Grundwissen {
 	public static void main(String[] args) {
 		Klasse klasse1 = new Klasse("Revengers", "Basti");
-		
+
 		System.out.println(klasse1.name);
 		System.out.println(klasse1.lehrer);
 		System.out.println("Das ist ein Test!");
+		System.out.println("Das Auch!");
 	}
 }
