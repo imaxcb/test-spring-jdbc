0541cdad5137003a0aaa4da651013572ef48833e
MaxBauer <MaxBauer@wxp21101.dvs-plattling.de>
2017-11-09 13:50:57 +0100

    von MacBook

--- Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
+++ Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
@@ -3,7 +3,7 @@ package mbauer.Einfuehrung_Objektorientierung;
 
 public class Grundwissen {
 	public static void main(String[] args) {
-		Klasse klasse1 = new Klasse("Revengers", "Sepp");
+		Klasse klasse1 = new Klasse("Revengers", "Basti");
 
 		System.out.println(klasse1.name);
 		System.out.println(klasse1.lehrer);
