30077e06a6113c7eca19facfa6bf93f38f817ae2
MaxBauer <MaxBauer@wxp21101.dvs-plattling.de>
2017-11-09 13:58:31 +0100

    von MacBook

--- Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
+++ Einfuehrung_Objektorientierung/src/mbauer/Einfuehrung_Objektorientierung/Grundwissen.java
@@ -3,36 +3,10 @@ package mbauer.Einfuehrung_Objektorientierung;
 
 public class Grundwissen {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Klasse klasse1 = new Klasse("Revengers", "Basti");
-=======
-		Kuh elsa = new Kuh();
-		elsa.name = "elsa";
-		elsa.milchProTag = 30;
-		elsa.farbe = "lila und weiss";
-		elsa.preis.wert = 345.67;
->>>>>>> branch 'master' of https://github.com/imaxcb/test-spring-jdbc
-
-<<<<<<< HEAD
+		
 		System.out.println(klasse1.name);
 		System.out.println(klasse1.lehrer);
-=======
-		// Euro elsaPreis = new Euro();
-		// elsaPreis.wert = 123.78;
-		// elsa.preis = elsaPreis;
->>>>>>> branch 'master' of https://github.com/imaxcb/test-spring-jdbc
 
-<<<<<<< HEAD
-=======
-		Kuh Gerlinde = new Kuh();
-		Gerlinde.name = "Gerlinde";
-		Gerlinde.milchProTag = 0;
-		Gerlinde.preis.wert = 9001.00;
-		Gerlinde.farbe = "braun";
-
-		System.out.println(elsa.preis);
-		System.out.println(Gerlinde.preis);
-		// System.out.println(elsaPreis);
->>>>>>> branch 'master' of https://github.com/imaxcb/test-spring-jdbc
 	}
 }
